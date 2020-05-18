c41374ccde4fa34ab839cf216cad4b6d32a48e0c
leesak <yo1603@naver.com>
2020-05-18 18:52:15 +0900

    Modified templates to display posts from datavase.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+# post_list를 view에서 보여줌
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
