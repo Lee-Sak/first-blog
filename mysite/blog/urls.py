287eba4dde01ce2a49f4d2e4698f9dd0341918c1
leesak <yo1603@naver.com>
2020-05-18 21:04:43 +0900

    added view and template for detailed blog post as well as css for the site

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 # 누군가 해당 웹사이트에 들어왔을 때 views.post_list를 보여주라고 말함
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
